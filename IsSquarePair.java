d7ddedb8f40ef2ebbe89c1e1ba6632457e50f830
Valeria Chikina <dliavajnogo@gmail.com>
2025-09-13 18:08:24 +0300

    Add user input validation

--- IsSquarePair.java
+++ IsSquarePair.java
@@ -1,18 +1,25 @@
 import java.util. Scanner;
 
 public class IsSquarePair {
-    public static void main(String[] args){
-        
+   public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        System.out.println(" Give the first integer:");
-        int firstInt = Integer.parseInt(scan.nextLine());
-
-        System.out.println(" Give the second integer:");
-        int secondInt = Integer.parseInt(scan.nextLine());
+        
+        int firstInt = getValidInt(scan, "Give the first integer:");
+        int secondInt = getValidInt(scan, "Give the second integer:");
 
         System.out.println("Your pair of numbers " + (isSquarePair(firstInt, secondInt) ? "is perfect square pair!" : "is not perfect square pair!"));
         scan.close();
+    }
+    
+    public static int getValidInt(Scanner scanner, String prompt) {
+        while (true) { // Infinite loop until valid input is received
+            System.out.println(prompt);
+            try {
+                return Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter an integer.");
+            }
+        }
     }
     
     public static boolean isSquarePair(int first, int second){
