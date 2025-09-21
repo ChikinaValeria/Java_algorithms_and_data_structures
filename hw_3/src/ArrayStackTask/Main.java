be01e212056db002b71e9e97e305665ec3182197
Valeria Chikina <dliavajnogo@gmail.com>
2025-09-21 18:13:54 +0300

    Added check of ExpandCapacity method

--- hw_3/src/ArrayStackTask/Main.java
+++ hw_3/src/ArrayStackTask/Main.java
@@ -4,7 +4,7 @@ import ArrayStackTask.exceptions.EmptyCollectionException;
 
 public class Main {
     public static void main(String[] args) {
-        StackADT<String> stack = new ArrayStack<>();
+        StackADT<String> stack = new ArrayStack<>(3);
 
         System.out.println("Checking if the stack is empty at the beginning: " + stack.isEmpty());
         System.out.println("Current stack size: " + stack.size());
@@ -14,10 +14,12 @@ public class Main {
         stack.push("Second element");
         stack.push("Third element");
 
-        System.out.println("Checking if the stack is empty after adding: " + stack.isEmpty());
-        System.out.println("Current stack size: " + stack.size());
+        System.out.println("Current stack size before adding a new element: " + stack.size());
 
-        // Using the toString() method to see the stack's content
+        System.out.println("Adding a fourth element, which should trigger capacity expansion...");
+        stack.push("Fourth element");
+
+        System.out.println("Current stack size after expansion: " + stack.size());
         System.out.println("Stack content: " + stack.toString());
 
         try {
@@ -25,8 +27,9 @@ public class Main {
             System.out.println("Removing the top element: " + stack.pop());
             System.out.println("Removing the top element: " + stack.pop());
             System.out.println("Removing the top element: " + stack.pop());
+            System.out.println("Removing the top element: " + stack.pop());
             System.out.println("\nRemoving one more element...");
-            // This line will cause an exception because the stack is empty
+
             System.out.println("Top element: " + stack.pop());
         } catch (EmptyCollectionException e) {
             System.out.println("An error occurred: " + e.getMessage());
