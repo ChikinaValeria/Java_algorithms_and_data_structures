42f7dea38e1de7f9b9d907280f565f7d23ec6dbb
Valeria Chikina <dliavajnogo@gmail.com>
2025-09-21 12:03:14 +0300

    Enhenced user imput experience in ReverseIntegersUsingStack

--- ReverseIntegersUsingStack.java
+++ ReverseIntegersUsingStack.java
@@ -2,33 +2,40 @@ import java.util.Stack;
 import java.util.Scanner;
 
 public class ReverseIntegersUsingStack {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Stack<Integer> stack = new Stack<>();
+        boolean inputIsValid = false;
 
-        System.out.println("Enter integers separated by spaces and in the end feed <enter>:");
-        
-        String inputLine = scanner.nextLine();
-        String[] numbersAsString = inputLine.split(" ");
-        
-        for (String s : numbersAsString) {
+        while (!inputIsValid) {
+            System.out.println("Enter integers separated by spaces and in the end feed <enter>:");
+            String inputLine = scanner.nextLine();
+            String[] numbersAsString = inputLine.split(" ");
+            
+            if (inputLine.trim().isEmpty()) {
+                System.out.println("Error: Input cannot be empty. Please try again.");
+                continue;// restartung the cycle
+            }
+
             try {
-                int number = Integer.parseInt(s);
-                stack.push(number);
+                for (String s : numbersAsString) {
+                    int number = Integer.parseInt(s);
+                    stack.push(number);
+                }
+                inputIsValid = true;
             } catch (NumberFormatException e) {
                 System.out.println("Error: One of the inputs was not a valid integer. Please try again.");
-                scanner.close();
-                return;
+                stack.clear();
             }
         }
 
         System.out.println("\nReversed order:");
-        
-         while (!stack.isEmpty()) {
+        while (!stack.isEmpty()) {
             System.out.print(stack.pop() + " ");
         }
-        System.out.println(); 
+        System.out.println();
 
         scanner.close();
     }
 }
+
