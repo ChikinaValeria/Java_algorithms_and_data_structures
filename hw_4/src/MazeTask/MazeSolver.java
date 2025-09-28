c4d7619585f23a2d5ecf86b0017f4b759522518e
Valeria Chikina <dliavajnogo@gmail.com>
2025-09-28 11:51:07 +0300

    Delete colors

--- hw_4/src/MazeTask/MazeSolver.java
+++ hw_4/src/MazeTask/MazeSolver.java
@@ -30,43 +30,42 @@ public class MazeSolver
      * @param column column index of current location
      * @return true if the maze has been solved
      */
-    public boolean traverse()
-    {
+    public boolean traverse() {
         System.out.println("\nStarting maze traversal with a stack...");
         boolean done = false;
         PositionNode pos = new PositionNode(0, 0, null);
-        Deque<PositionNode> stack = new LinkedList<PositionNode>();
+        Deque<PositionNode> stack = new LinkedList<>();
         stack.push(pos);
 
-        while (!(done) && !stack.isEmpty())
-    {
-        pos = stack.pop();
-        System.out.println("Exploring position: (" + pos.getx() + ", " + pos.gety() + ")");
-        maze.tryPosition(pos.getx(), pos.gety());
-        if (pos.getx() == maze.getRows() - 1 && pos.gety() == maze.getColumns() - 1)
-        {
-            done = true;
-            System.out.println("Path found! Tracing back the solution...");
-
-            PositionNode current = pos;
-            while (current != null) {
-                maze.markPath(current.getx(), current.gety());
-                current = current.getParent();
-            }
-        }
-        else
-        {
-            System.out.println("  Checking neighbors...");
-            push_new_pos(pos.getx() - 1, pos.gety(), stack, pos); // Изменено
-            push_new_pos(pos.getx() + 1, pos.gety(), stack, pos); // Изменено
-            push_new_pos(pos.getx(), pos.gety() - 1, stack, pos); // Изменено
-            push_new_pos(pos.getx(), pos.gety() + 1, stack, pos); // Изменено
-        }
+        System.out.println("Initial state:");
+        System.out.println(maze);
         printStackContents(stack);
-    }
+
+        while (!done && !stack.isEmpty()) {
+            pos = stack.pop();
+            System.out.println("Exploring position: (" + pos.getx() + ", " + pos.gety() + ")");
+            maze.tryPosition(pos.getx(), pos.gety());
+
+            if (pos.getx() == maze.getRows() - 1 && pos.gety() == maze.getColumns() - 1) {
+                done = true;
+                System.out.println("Path found! Tracing back the solution...");
+                // Step by step visualization of marking Path
+                traceAndMarkPath(pos);
+                break;
+            } else {
+                System.out.println("  Checking neighbors...");
+                push_new_pos(pos.getx() - 1, pos.gety(), stack, pos);
+                push_new_pos(pos.getx() + 1, pos.gety(), stack, pos);
+                push_new_pos(pos.getx(), pos.gety() - 1, stack, pos);
+                push_new_pos(pos.getx(), pos.gety() + 1, stack, pos);
+            }
+            System.out.println("\n--- Current Maze State ---");
+            System.out.println(maze);
+            printStackContents(stack);
+        }
         System.out.println("Traversal finished.");
         return done;
-}
+    }
 
     private void push_new_pos(int x, int y, Deque<PositionNode> stack, PositionNode currentParent)
     {
@@ -81,7 +80,7 @@ public class MazeSolver
 
     private void printStackContents(Deque<PositionNode> stack) {
         System.out.print("Current Stack: [");
-        // Используем итератор для безопасного прохождения по стеку
+
         Iterator<PositionNode> iterator = stack.iterator();
         while (iterator.hasNext()) {
             PositionNode pos = iterator.next();
@@ -93,7 +92,6 @@ public class MazeSolver
         System.out.println("]\n");
     }
 
-    // Новый метод для пошаговой визуализации обратного хода
     private void traceAndMarkPath(PositionNode endNode) {
         PositionNode current = endNode;
         while (current != null) {
@@ -102,7 +100,6 @@ public class MazeSolver
             System.out.println(maze);
             current = current.getParent();
             try {
-                // Добавим небольшую задержку для лучшей визуализации
                 Thread.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
