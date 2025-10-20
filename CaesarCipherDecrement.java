4b65aac279fd6dd71e7c608a2c51d691bcac146e
Valeria Chikina <dliavajnogo@gmail.com>
2025-10-20 10:32:01 +0300

    Add cipher using decrement by 1

---
+++ CaesarCipherDecrement.java
@@ -0,0 +1,36 @@
+/**
+ * Codes demonstrates a simple Caesar cipher (shift -1) to encrypt and decrypt messages.
+ */
+public class CaesarCipherDecrement
+{
+    /**
+     * Encode and decode a message using a constant key of -1.
+     */
+    public static void main(String[] args)
+    {
+        // The key value is set to -1 for all characters
+        final int KEY_VALUE = -1;
+
+        String encoded = "";
+        String decoded = "";
+        String message = "university";
+
+        System.out.println("Original Message:\n" + message + "\n");
+
+        // Encoding
+        for (int scan = 0; scan < message.length(); scan++)
+        {
+            encoded += (char) (message.charAt(scan) + KEY_VALUE);
+        }
+
+        System.out.println ("Encoded Message:\n" + encoded + "\n");
+
+        // Decoding
+        for (int scan = 0; scan < encoded.length(); scan++)
+        {
+            decoded += (char) (encoded.charAt(scan) - KEY_VALUE);
+        }
+
+        System.out.println ("Decoded Message:\n" + decoded);
+    }
+}
