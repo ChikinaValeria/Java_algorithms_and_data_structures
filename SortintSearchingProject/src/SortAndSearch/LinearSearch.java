850a8705e0920deb7b762124b78ec5432b862978
Valeria Chikina <dliavajnogo@gmail.com>
2025-12-05 09:42:46 +0200

    Add comments

--- SortintSearchingProject/src/SortAndSearch/LinearSearch.java
+++ SortintSearchingProject/src/SortAndSearch/LinearSearch.java
@@ -5,7 +5,7 @@ public class LinearSearch {
     private static final int[] SEARCH_LIST = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     public static String search(int targetValue) {
-
+        
         for (int i = 0; i < SEARCH_LIST.length; i++) {
             int currentElement = SEARCH_LIST[i];
             if (currentElement == targetValue) {
